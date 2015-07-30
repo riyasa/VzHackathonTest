6235c9134b362ff5b55e406f9f05027f011d4f34
mohamed.alungal@verizon.com <mohamed.alungal@verizon.com>
2015-07-30 00:33:38 -0700

    test1 file

---
+++ test/com/hackathon/service/ServiceTest1.java
@@ -0,0 +1,14 @@
+package com.hackathon.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServiceTest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
