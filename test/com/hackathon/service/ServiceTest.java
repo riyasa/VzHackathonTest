efdb44a06169e289fdbb367aa0ca76cc8c013179
mohamed.alungal <mohamed.alungal@verizon.com>
2015-07-30 08:43:23 +0530

    added service test file

---
+++ test/com/hackathon/service/ServiceTest.java
@@ -0,0 +1,19 @@
+package com.hackathon.service;
+
+import static org.junit.Assert.*;
+
+import javax.xml.ws.BindingType;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ServiceTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
