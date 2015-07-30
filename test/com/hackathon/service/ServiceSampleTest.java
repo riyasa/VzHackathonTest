589ba313eec5039494d0bfb21808bf5b65a635f4
mohamed.alungal <mohamed.alungal@verizon.com>
2015-07-30 07:52:33 +0530

    added service sample test

---
+++ test/com/hackathon/service/ServiceSampleTest.java
@@ -0,0 +1,14 @@
+package com.hackathon.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServiceSampleTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
