09ca8edc232293fd92fb12967c349c005f851535
mohamed.alungal@verizon.com <mohamed.alungal@verizon.com>
2015-07-30 00:22:49 -0700

    service file changes

--- test/com/hackathon/service/ServiceTest.java
+++ test/com/hackathon/service/ServiceTest.java
@@ -3,7 +3,10 @@ package com.hackathon.service;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceTest {
 
 	@Test
